1c92558c76ef6aad04afe1d90e2715731623a1ff
n-kev1n <kevin_@tuta.io>
2023-11-02 11:34:22 +0100

    registered routes

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,20 @@
 import Fastify from "fastify";
 import { logger } from "./logger";
+import { articleRoutes } from "../modules/article/article.routes";
 
 export function buildServer() {
   const server = Fastify({
     logger,
   });
+
+  /*
+   * TODO: Register plugins.
+   */
+
+  /*
+   * INFO: Register routes.
+   */
+  server.register(articleRoutes, { prefix: "/api/v1/articles" });
 
   return server;
 }
