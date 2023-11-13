853ea83ee01cb83ebc29c7fdb453d4836bbff7c5
n-kev1n <kevin_@tuta.io>
2023-11-13 23:53:23 +0100

    imported create article handler

--- src/modules/article/article.routes.ts
+++ src/modules/article/article.routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { createArticleHander } from "./article.controller";
 
 export async function articleRoutes(app: FastifyInstance) {
   // Create an article.
-  app.post("/", {}, () => {});
+  app.post("/", {}, createArticleHander);
 
   // Get Articles.
   app.get("/", {}, () => {});
