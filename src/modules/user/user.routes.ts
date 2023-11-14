17831cb31df56e532d5c14a06f883b4e1a50160f
n-kev1n <kevin_@tuta.io>
2023-11-14 23:53:17 +0100

    added route for fetching and creating users

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from "fastify";
+import { TUserSchema } from "./user.schema";
+import { createUserHandler, fetchUserHandler } from "./user.controller";
 
 export async function userRoutes(app: FastifyInstance) {
+  // Get User.
+  app.get("/", fetchUserHandler);
+
   // User sign up
   // TODO: add schema and controller
-  app.post("/register", {}, () => {});
+  app.post("/register", { schema: TUserSchema }, createUserHandler);
 
   // User Sign in
   // TODO: add schema and controller
