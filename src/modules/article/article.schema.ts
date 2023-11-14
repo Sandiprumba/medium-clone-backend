ef5b9ecf66c6677f43ef072266b8ae24eebe89e5
n-kev1n <kevin_@tuta.io>
2023-11-14 23:54:00 +0100

    added JSON schemas

--- src/modules/article/article.schema.ts
+++ src/modules/article/article.schema.ts
@@ -1,3 +1,6 @@
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+
 export type TArticle = {
   id: string;
   title: string;
@@ -6,4 +9,22 @@ export type TArticle = {
   published: boolean;
   markdown: string;
   authorId: string;
+};
+
+const createArticleBodySchema = z.object({
+  title: z.string({
+    required_error: "Article title is required",
+    invalid_type_error: "Invalid type",
+  }),
+  summary: z.string(),
+  image: z.string(),
+  published: z.boolean(),
+  markdown: z.string(),
+  authorId: z.string(),
+});
+
+export type TArticleBody = z.infer<typeof createArticleBodySchema>;
+
+export const TArticleSchema = {
+  body: zodToJsonSchema(createArticleBodySchema, "TArticleSchema"),
 };
