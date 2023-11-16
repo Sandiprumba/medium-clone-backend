3733a765cd26724973764118c1f93a1926c5f200
n-kev1n <kevin_@tuta.io>
2023-11-16 23:10:30 +0100

    added controller to fetch one user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { TUserBody } from "./user.schema";
-import { createUser, fetchUsers } from "./user.service";
+import { createUser, fetchOneUser, fetchUsers } from "./user.service";
 import { logger } from "../../utils/logger";
 
 export async function createUserHandler(
@@ -19,8 +19,27 @@ export async function createUserHandler(
   }
 }
 
-export async function fetchUserHandler() {
+export async function fetchUsersHandler() {
   const users = await fetchUsers();
 
   return users;
+}
+
+export async function fetchOneUserHander(
+  req: FastifyRequest<{
+    Params: { id: string };
+  }>,
+  res: FastifyReply,
+) {
+  const { id } = req.params;
+  if (!id) return res.code(400).send({ message: "Invalid article ID" });
+
+  try {
+    const user = await fetchOneUser(id);
+    res.code(200).send(user);
+  } catch (e) {
+    logger.error(e);
+
+    res.code(500).send(e);
+  }
 }
